733b43f2820f15ce28f59c2f2686d9ca16568c3b
Ольга <olya.jl@yandex.ru>
2017-09-25 13:27:40 +0300

    Task 2, 3, 6

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -60,8 +60,8 @@ fun seconds(hours: Int, minutes: Int, seconds: Int): Int = hours * 3600 + minute
  * Определить длину того же отрезка в метрах (в данном случае 18.98).
  * 1 сажень = 3 аршина = 48 вершков, 1 вершок = 4.445 см.
  */
-fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (vershoks * 0,00445) + (arshins * 48 * 0,00445) +
-(sagenes * 3 * 48 * 0,00445)
+fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (vershoks * 0.00445) + (arshins * 48 * 0.00445) +
+(sagenes * 3 * 48 * 0.00445)
 
 /**
  * Тривиальная
@@ -69,7 +69,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = (vershok
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(grad: Int, min: Int, sec: Int): Double = ((grad + (min + sec / 60) / 60) * 3.14) / 180
+fun angleInRadian(grad: Int, min: Int, sec: Int): Double = ((grad + (min + sec / 60.0) / 60.0) * 3.14) / 180
 
 /**
  * Тривиальная
@@ -98,7 +98,8 @@ fun thirdDigit(number: Int): Int = TODO()
  * прибыл на станцию назначения в h2 часов m2 минут того же дня (например в 13:01).
  * Определите время поезда в пути в минутах (в данном случае 216).
  */
-fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = TODO()
+fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minutesArrive: Int): Int = (hoursArrive * 60 - hoursDepart * 60) +
+        (minutesArrive - minutesDepart)
 
 /**
  * Простая
