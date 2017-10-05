cee8802be33179bef62b5c3a4ceb963ecfceb513
Ольга <olya.jl@yandex.ru>
2017-10-05 22:04:32 +0300

    lesson3

--- src/lesson3/task1/Loop.kt
+++ src/lesson3/task1/Loop.kt
@@ -60,13 +60,8 @@ fun digitCountInNumber(n: Int, m: Int): Int =
  * Найти количество цифр в заданном числе n.
  * Например, число 1 содержит 1 цифру, 456 -- 3 цифры, 65536 -- 5 цифр.
  */
-fun digitNumber(n: Int): Int {
-    var result = 0
-    for (i in 1..n) {
-        result = result + 1
-    }
-    return result
-}
+fun digitNumber(n: Int): Int = n.toString().length
+
 /**
  * Простая
  *
