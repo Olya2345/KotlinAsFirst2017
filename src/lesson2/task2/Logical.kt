9a7c160afbdf273f38c2aae7fc2234df28201805
Ольга <olya.jl@yandex.ru>
2017-10-08 23:16:10 +0300

    lesson2 task2

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -55,10 +55,9 @@ fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
     var x = a
     var y = b
     if ((x <= y) && (c < y)) y = c
-    if ((y < x) && (c < x)) x = c
-    return if (((x <= r) && (y <= s)) || (x <= s) && (y <= r)) true
-    else false
-
+    if ((y <= x) && (c < x)) x = c
+    return if (((r < x) || (s < y)) && ((r < y) || (s < x))) false
+    else true
     /*((((a <= r) || (b <= r)) && (c <= s)) || (((a <= s) || (b <= s)) && (c <= r)) || (((a <= r) || (c <= r)) && (b <= s)) ||
             (((a <= s) || (c <= s)) && (b <= r)) || (((b <= s) || (c <= s)) && (a <= r)) || (((b <= r) || (c <= r)) && (a <= s)))
 */
