733249099dd53090b43ecfd45e6ba61e900178fb
Olga <olya.jl@yandex.ru>
2017-11-22 22:06:10 +0300

    lesson 4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -233,11 +233,14 @@ fun factorizeToString(n: Int): String {
 fun convert(n: Int, base: Int): List<Int> {
     var p = listOf<Int>()
     var m = n
-    for (i in 1..m) {
-        p += m % base
-        if (m < base) break
-        else m /= base
+    if (n != 0) {
+        for (i in 1..m) {
+            p += m % base
+            if (m < base) break
+            else m /= base
+        }
     }
+    else p.plus(0)
     return p.reversed()
 }
 
@@ -254,7 +257,7 @@ fun convertToString(n: Int, base: Int): String {
     var list = listOf<String>("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z")
     var c = n
     for (i in 1..c) {
-        if (c % base > 9) m += list.elementAt(c % base - 1)
+        if (c % base > 9) m += list.elementAt(c % base - 11)
         else m += c % base
         if (c < base) break
         else c /= base
